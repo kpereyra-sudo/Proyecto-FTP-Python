1b8b0b14559d1bd76bb334be69622183418593bf
Katherine Pereyra <kpereiraestrada@gmail.com>
2020-12-23 09:17:19 -0400

    Se limpio el codigo

--- FileFTPFuntions.py
+++ FileFTPFuntions.py
@@ -14,18 +14,8 @@ def ConeCTOftp(ip):
     except Exception as e:
         print(e)
   
-    # try:
+    
 
-    # # Ir al directorio especificado
-    #     ftp.cwd('ej/j')
-    # # acceder a los archivos del directorio
-    #     lista_journal = ftp.nlst()
-        
-    #     return lista_journal
-        
-    # except Exception as e:
-    #     print(e)
-ftp = ConeCTOftp('10.10.19.130')
 
 def ConecTOFTPMicrosoft(ip, directory):
 
@@ -38,7 +28,7 @@ def ConecTOFTPMicrosoft(ip, directory):
         print("Problemas con la ip o la caja esta apagada")
     return ftp
 
-# ftp = ConeCTOftp('10.10.19.130')
+
     
 def  GotoDirectory(ftp, directorio):
     
@@ -58,19 +48,7 @@ def  GotoDirectory(ftp, directorio):
 
  
 
-lista_journal = GotoDirectory( ftp, "ej/j")
-
-    # try:
-
-    # # Ir al directorio especificado
-    #     print(ftp.cwd(directorio))
-    # # acceder a los archivos del directorio
-    #     lista_journal = ftp.nlst()
-        
-
-
-    # except:
-    #     print("ESE DIRECTORIO NO EXISTE")
+    
 
 def insertar_ruta_nas(ftp, journal, ruta):
 
@@ -88,10 +66,7 @@ def InsertarRutaFTP(journal, servidor, directorio, local_filename):
    
     try:
 
-    # Ir al directorio especificado
-        
-    #acceder a los archivos del directorio
-        # local_filename = "//10.10.1.67/Volume_1/Journal y Lventas/Journal/2020/516 Galerias 360/%s" %journal
+
         fp = open(local_filename, 'rb')
         ftpj.storlines('STOR %s' %journal, fp)
         fp.close()
@@ -99,23 +74,7 @@ def InsertarRutaFTP(journal, servidor, directorio, local_filename):
 
     except Exception as e:
         print(e)
-        
-# #ir por cada journal y si no esta encriptado enviarlo a las rutas especificadas.
-# # journal = (journal for journal in lista_journal if len(journal) == 7
-
-#     # if len(journal) == 7 :
-#         #insert NAS
-# insertar_ruta_nas(r"\\10.10.1.67\Volume_1\Journal y Lventas\Journal\2020\516 Galerias 360")
-#         #Insert on My desk
-# InsertarRutaFTP(journal)    
-    
 
 
-if __name__ == "__main__":
-    for journal in lista_journal:
-        if len(journal) == 7 :
-        # insert NAS
-            insertar_ruta_nas(r"\\10.10.1.67\Volume_1\Journal y Lventas\Journal\2020\516 Galerias 360")
-        #Insert on My desk
-            InsertarRutaFTP(journal, 'mlddcdofls01-vm.datacenter.milady.local', '/journal/Journal Generales/516', "//10.10.1.67/Volume_1/Journal y Lventas/Journal/2020/516 Galerias 360/%s" %journal) 
+
     
