555a1f0e7b291ce6e30e26f7588c21d0cfaad197
Katherine Pereyra <kpereiraestrada@gmail.com>
2020-12-24 21:13:07 -0400

    Se agrego 516

--- Tienda360.py
+++ Tienda360.py
@@ -1,86 +1,41 @@
-import ftplib  as f
-import os
-from datetime import datetime, timedelta
+import FileFTPFuntions as p
+import json
 
 
-# def ConeCTOftp(ip,directorio):
+with open('Config.json', 'r') as file:
+    config = json.load(file)
 
-try:
-        ftp = f.FTP('10.10.19.130')
-        print(ftp.welcome)
-        ftp.login()
-except:
-        print("Problemas con la ip o la caja esta apagada")
+# lee la ip desde el config file
+IpCaja = config['TiendaFTP516']['ipcaja']
+# leer ruta nas y la concatena con cada journal subido.
+NasPATHFile = config['TiendaFTP516'][ "pathfile"]
+#Lee el servidor especificado en este caso el milady del FTP
+ServiDORMilady = config['TiendaFTP516'][ "Servidor"]
+# Lee la ruta FTP hacia donde dirigira los Journal
+RutaFTP = config['TiendaFTP516']["RutaFTP"]
+#Lee ruta NAS
+RutaNAS = config['TiendaFTP516']["RutaNAS"]
+#Lee la ruta de journal de la caja
+JournalDirectoryCaja = config['CajasFTP']["journal"]
 
-try:
+def Galerias360():
 
-    # Ir al directorio especificado
-        print(ftp.cwd('ej/j'))
-    # acceder a los archivos del directorio
-        lista_journal = ftp.nlst()
-        
+    """ Lee las funciones que conectan con el FTP, reciben los parametros de conexion
+    y extrae cada journal de la caja y los insertar en el nas y el ftp de los journal. """
 
+    ftp = p.ConeCTOftp(IpCaja)
+    lista_journal = p.GotoDirectory(ftp, JournalDirectoryCaja)
 
-except:
-        print("ESE DIRECTORIO NO EXISTE")
-
-# ConeCTOftp('10.10.19.130', 'ej/j') 
-
-def insertar_ruta_nas(ruta):
-
-    """ Escribir la ruta que se especifica como parametro en la funcion y guarda los Journal alla"""
-    local_filename = os.path.join(ruta, journal)
-    lf = open(local_filename, "wb")
-    ftp.retrbinary("RETR " + journal, lf.write, 8*1024)
-
-
-def InsertarRutaFTP(journal):
-
-    try:
-        ftp = f.FTP('mlddcdofls01-vm.datacenter.milady.local')
-        print(ftp.welcome)
-        ftp.login('unicomer\katherine_pereyra', 'Kper0404')
-    except:
-        print("Problemas con la conexion o debe cambiar el password del user")
-
-
-
-       
-
-    try:
-
-    # # Ir al directorio especificado
-        ftp.cwd('/journal/Journal Generales/516')
-    # #acceder a los archivos del directorio
-   
-        local_filename = "//10.10.1.67/Volume_1/Journal y Lventas/Journal/2020/516 Galerias 360/%s" %journal
-        fp = open(local_filename, 'rb')
-        ftp.storlines('STOR %s' %journal, fp)
-        fp.close()
-
-        
-            
-    except Exception as e:
-        print(e)
-
-
-
-#ir por cada journal y si no esta encriptado enviarlo a las rutas especificadas.
-for journal in lista_journal:
-
-    if len(journal) == 7 :
-        #insert NAS
-        insertar_ruta_nas(r"\\10.10.1.67\Volume_1\Journal y Lventas\Journal\2020\516 Galerias 360")
+    for journal in lista_journal:
+        if len(journal) == 7 :
+        # insert NAS
+            nas = p.insertar_ruta_nas(ftp, journal, RutaNAS)
         #Insert on My desk
-        InsertarRutaFTP(journal)
-
-
-        
-print("Succesfull Upload Files")
+            InsertFTP = p.InsertarRutaFTP(journal, ServiDORMilady, RutaFTP, NasPATHFile %journal )
 
 
 
-
-
-
+if __name__ == "__main__":
+  Galerias360 = Galerias360()
+   
 
